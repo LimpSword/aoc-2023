047b5877fef9392e6368441714de983673525a1e
LimpSword <contact@limpsword.fr>
2023-12-01 17:37:28 +0100

    Day 1 : mad optimization

--- day1/day1.go
+++ day1/day1.go
@@ -83,3 +83,97 @@ func Solve2() int {
 	}
 	return sum
 }
+
+func Solve2Mad() int {
+	bytes, _ := os.ReadFile("input.txt")
+	input := string(bytes)
+
+	// Part 2
+	lines := strings.Split(input, "\n")
+	sum := 0
+
+	for _, line := range lines {
+		first, last := -1, -1
+		for i := 0; i < len(line); i++ {
+			if unicode.IsDigit(rune(line[i])) {
+				first = int(line[i] - '0')
+				break
+			} else {
+				if i+2 < len(line) && line[i:i+3] == "one" {
+					first = 1
+					i++
+					break
+				} else if i+2 < len(line) && line[i:i+3] == "two" {
+					first = 2
+					i += 2
+					break
+				} else if i+4 < len(line) && line[i:i+5] == "three" {
+					first = 3
+					i += 4
+					break
+				} else if i+3 < len(line) && line[i:i+4] == "four" {
+					first = 4
+					i += 3
+					break
+				} else if i+3 < len(line) && line[i:i+4] == "five" {
+					first = 5
+					i += 3
+					break
+				} else if i+2 < len(line) && line[i:i+3] == "six" {
+					first = 6
+					i += 2
+					break
+				} else if i+4 < len(line) && line[i:i+5] == "seven" {
+					first = 7
+					i += 4
+					break
+				} else if i+4 < len(line) && line[i:i+5] == "eight" {
+					first = 8
+					i += 4
+					break
+				} else if i+3 < len(line) && line[i:i+4] == "nine" {
+					first = 9
+					i += 3
+					break
+				}
+			}
+		}
+		for i := len(line) - 1; i >= 0; i-- {
+			if unicode.IsDigit(rune(line[i])) {
+				last = int(line[i] - '0')
+				break
+			} else {
+				if i >= 2 && line[i-2:i+1] == "one" {
+					last = 1
+					break
+				} else if i >= 2 && line[i-2:i+1] == "two" {
+					last = 2
+					break
+				} else if i >= 4 && line[i-4:i+1] == "three" {
+					last = 3
+					break
+				} else if i >= 3 && line[i-3:i+1] == "four" {
+					last = 4
+					break
+				} else if i >= 3 && line[i-3:i+1] == "five" {
+					last = 5
+					break
+				} else if i >= 2 && line[i-2:i+1] == "six" {
+					last = 6
+					break
+				} else if i >= 4 && line[i-4:i+1] == "seven" {
+					last = 7
+					break
+				} else if i >= 4 && line[i-4:i+1] == "eight" {
+					last = 8
+					break
+				} else if i >= 3 && line[i-3:i+1] == "nine" {
+					last = 9
+					break
+				}
+			}
+		}
+		sum += 10*first + last
+	}
+	return sum
+}
--- day1/day1_test.go
+++ day1/day1_test.go
@@ -21,3 +21,12 @@ func BenchmarkDay2(b *testing.B) {
 		}
 	}
 }
+
+func BenchmarkDay2Mad(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		r := Solve2Mad()
+		if r != 54706 {
+			b.Errorf("Got %d, expected 54706", r)
+		}
+	}
+}
